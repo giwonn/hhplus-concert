fb1b1390bd2db13bad6e00f09af868fa381674e1
Allen / 임기원 <l4279625@gmail.com>
2025-01-24 07:38:51 +0900

    fix: Lock 파라미터 파싱 fixed

--- src/main/java/kr/hhplus/be/server/annotation/simplelock/SimpleLockAspect.java
+++ src/main/java/kr/hhplus/be/server/annotation/simplelock/SimpleLockAspect.java
@@ -7,8 +7,15 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 
 @RequiredArgsConstructor
@@ -18,10 +25,12 @@ import org.springframework.stereotype.Component;
 public class SimpleLockAspect {
 
 	private final LockProvider lockProvider;
+	private final ExpressionParser parser = new SpelExpressionParser();
 
 	@Around("@annotation(simpleLock)")
 	public Object simpleLock(ProceedingJoinPoint joinPoint, SimpleLock simpleLock) throws Throwable {
-		boolean lock = lockProvider.lock(simpleLock.key(), 0, simpleLock.expireTime(), simpleLock.timeUnit());
+		String lockKey = evaluateKey(joinPoint, simpleLock.key());
+		boolean lock = lockProvider.lock(lockKey, 0, simpleLock.expireTime(), simpleLock.timeUnit());
 		if (!lock) throw new CustomException(RequestErrorCode.FAIL_REQUEST);
 
 		try {
@@ -29,5 +38,17 @@ public class SimpleLockAspect {
 		} finally {
 			lockProvider.unlock(simpleLock.key());
 		}
+	}
+
+	private String evaluateKey(ProceedingJoinPoint joinPoint, String keyExpression) {
+		String[] paramNames = ((MethodSignature) joinPoint.getSignature()).getParameterNames();
+		Object[] args = joinPoint.getArgs();
+
+		EvaluationContext context = new StandardEvaluationContext();
+		for (int i = 0; i < paramNames.length; i++) {
+			context.setVariable(paramNames[i], args[i]); // 변수 이름과 값 매핑
+		}
+
+		return parser.parseExpression(keyExpression).getValue(context, String.class);
 	}
 }
