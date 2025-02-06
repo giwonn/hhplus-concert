1926764d4c4255ff4d1745ad6a42571ea14e5707
Allen / 임기원 <l4279625@gmail.com>
2025-02-07 03:13:42 +0900

    chore: 부하테스트 세팅 개선

---
+++ k6/waiting-queue.js
@@ -0,0 +1,34 @@
+import http from "k6/http";
+import { check } from "k6";
+import { Counter } from "k6/metrics";
+
+export const options = {
+  stages: [
+    { duration: "5s", target: 100 },
+    { duration: "5s", target: 50 },
+    { duration: "5s", target: 10 },
+    { duration: "5s", target: 5 },
+  ],
+};
+
+const successCount = new Counter("success_count");
+const failCount = new Counter("fail_count");
+
+export default () => {
+  const url = "http://host.docker.internal:8080/queue/tokens";
+  const payload = JSON.stringify({ userId: 1 });
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    }
+  };
+  const response = http.post(url, payload, params);
+  check(response, { "status is 200": (r) => r.status === 200 });
+
+  if (response.status === 200) {
+    successCount.add(1);
+  } else {
+    failCount.add(1);
+  }
+}
+
--- src/main/java/kr/hhplus/be/server/core/config/WebConfig.java
+++ src/main/java/kr/hhplus/be/server/core/config/WebConfig.java
@@ -4,11 +4,9 @@ import kr.hhplus.be.server.api.queue.presentation.interceptor.ExpireQueueInterce
 import kr.hhplus.be.server.api.queue.presentation.interceptor.QueueValidationInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Profile("!load-test") // TODO : 대기열 없이 부하테스트 진행하기 위해 임시로 비활성화함. 추후 대기열 개선하여 해당 설정 제거 예정
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
